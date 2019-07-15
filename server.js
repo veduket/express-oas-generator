4fb6541040f8e08357d4b70bcbffa38fa8c408d1
Matvey Pashkovskiy <matvey.pashkovskiy@f-secure.com>
2019-07-15 13:15:28 +0300

    refs #24 trying to reproduce the bug

--- server.js
+++ server.js
@@ -1,43 +1,33 @@
 // Example of using the module
 
-
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const generator = require('./index.js');
-const _ = require('lodash');
-const zlib = require('zlib');
 
 const app = express();
-generator.init(app, function (spec) {
-  _.set(spec, 'paths["/foo/{name}"].get.parameters[0].description', 'description of a parameter');
-  return spec;
-}, './test_spec.json');
+generator.init(app, {});
 
 app.use(bodyParser.json({}));
-let router = express.Router();
-router.route('/foo/stranger')
-      .get(function (req, res, next) {
-        //code here
-        console.log('calling /foo/stranger');
-        res.json({message: 'hello stranger'});
-        return next();
-      });
-router.route('/foo/:name')
-      .get(function (req, res, next) {
-        console.log('calling /foo/:name');
-        res.json({message: 'hello ' + req.params.name});
-        return next();
-      });
-router.route('/gzip')
-      .get(function (req, res, next) {
-        console.log('calling /gzip');
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Content-Encoding', 'gzip');
-        zlib.gzip(JSON.stringify({message: 'gzip'}), function (error, result) {
-          res.status(200).send(result);
-          return next();
-        })
-      });
-app.use(router);
-app.set('port', 8080);
-app.listen(app.get('port'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const router = express.Router();
+router.post('/', async(req, res, next) => {
+  console.log(req.body);
+  next();
+});
+
+const port = 3606
+app.use('/books', router);
+app.listen(port, function() {
+  console.log('app listening on port: ' + port);
+  setTimeout(() => {
+    request({
+      url: `http://localhost:${port}/books`,
+      method: 'POST',
+      headers: {'content-type' : 'application/json'},
+      body: JSON.stringify({data: 'data'})
+    });
+  }, 2000);
+});
